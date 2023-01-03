614416ecd02b308bca55736b1a30148d9380ded3
muralim <parkerviktorov094@gmail.com>
2023-01-03 12:25:17 +0100

    added about file

--- app.js
+++ app.js
@@ -3,18 +3,17 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
-
-    //res.send('<p>Home page</p>')
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
-    
-    //res.send('<p>About page</p>')
     res.sendFile('./views/about.html', { root: __dirname });
 });
 
