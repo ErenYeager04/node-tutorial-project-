4c499efe4886bf1424398a34e0265349796a6659
muralim <parkerviktorov094@gmail.com>
2023-01-03 09:58:04 +0100

    end of the tutorial 5

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash')
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  
+    // lodash
+  const num = _.random(0, 20);
+  console.log(num)
+
+  const greet = _.once(() => {
+    console.log('hello')
+  }
+)
+  greet();
+  greet();
 
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
+  let path = './views/';
+  switch (req.url) {
+    case '/':
+        path += 'index.html';
+        break;
+    case '/about':
+        path += 'about.html';
+        break;
+    default:
+        path += '404.html';
+        break;              
+  }
+
  // send an html file
-  fs.readFile('./views/index.html', (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
         console.log(err);
     } else {
         // res.write(data);
-        res.end()
+        res.end(data)
     }
   })
 
