69d6b71745d57d306dca6acc13d96164073babc8
muralim <parkerviktorov094@gmail.com>
2023-01-04 13:47:12 +0100

    getting and sving data 9

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
 
 // express app
 const app = express();
 
+// connect to mongodb
+const dbURI = 'mongodb+srv://netninja:ernisgindi@nodetuts.v99jbb3.mongodb.net/node-tuts?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+
+// middleware and static pages
+app.use(express.static('public'));
 
 app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+      .then((result) => {
+        res.send(result)
+      })
+    .catch(() => {
+        console.log(err);
+    })  
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+})
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('63b56d5211d663e91a6db9b1')
+      .then((result) => {
+        res.send(result)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+})
 
 
 app.get('/', (req, res) => {
